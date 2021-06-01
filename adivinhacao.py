678863389a94fca183a279ddbf8a24dc5cf69399
Pietro Henrique <henripietro@gmail.com>
2021-06-01 18:33:10 -0300

    projrto_python

---
+++ adivinhacao.py
@@ -0,0 +1,12 @@
+print("##########")
+print("Bem vindo ao jogo de advinhação")
+print("################")
+
+numero_secreto = 70
+
+chute = input("digite o seu numero: ")
+
+print("você digitou ", chute)
+
+
+
---
+++ task1.py
@@ -0,0 +1,18 @@
+# print("brasil","são paulo",5,"guarulhos")
+# pais = 'Brasil'
+
+# subst = "Python"
+# verbo = "é"
+# adjetivo = "fantástico"
+# print(subst, verbo, adjetivo, sep="_", end="!\n")
+#
+# dia = 15
+# mes = 10
+# ano = 2015
+# print(dia, mes, ano, sep="/")
+
+pais = "brasil"
+
+
+
+
